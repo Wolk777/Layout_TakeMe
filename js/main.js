36e0c17ce3cea5841ed840bbb8686dc923dbebd7
Wolk777 <Sokol.Vlad90@mail.ru>
2019-09-30 14:41:49 +0300

    add page title and first 3 sections

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// event handler when clicking on the drop-down list
+function DropDown(elem) {
+    this.dd = elem;
+    this.placeholder = this.dd.children('span');
+    this.opts = this.dd.find('ul.dropdown > li');
+    this.val = '';
+    this.index = -1;
+    this.initEvents();
+}
+DropDown.prototype = {
+    initEvents : function() {
+        let obj = this;
+        // add/remove class 'active'
+        obj.dd.on('click', function(event){
+            $(this).toggleClass('active');
+            return false;
+        });
+        // write data
+        obj.opts.on('click',function(){
+            let opt = $(this);
+            obj.val = opt.text();
+            obj.index = opt.index();
+            obj.placeholder.text(obj.val);
+        });
+    },
+    getValue : function() {
+        return this.val;
+    },
+    getIndex : function() {
+        return this.index;
+    }
+}
+
+$(function() {
+
+    let dd1 = new DropDown( $('#dd1') );
+    let dd2 = new DropDown( $('#dd2') );
+    // removes a class when clicked outside the list
+    $(document).click(function() {
+        $('.dropdownList').removeClass('active');
+    });
+
+});
+
+// slider
+
+$('.slider_box').slick({
+    dots:true,
+    arrows:false,
+    slidesToShow:3,
+    variableWidth: true,
+});       
